5b9a41ea98da7345bbd893f1544ab62d1becd47b
Cristhian Laurente <frontend.crstn@gmail.com>
2020-01-13 07:38:59 -0500

    Updates

---
+++ precache-manifest.17819678c31b1d052c6186ca7abeea1e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c7f52b72691c3241b1e6de752f9200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc06d798ade74df511ff",
+    "url": "/static/css/main.93c22231.chunk.css"
+  },
+  {
+    "revision": "298945361d7ae217e637",
+    "url": "/static/js/2.1bc60f51.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.1bc60f51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc06d798ade74df511ff",
+    "url": "/static/js/main.b8f851ac.chunk.js"
+  },
+  {
+    "revision": "9f6cab67342d45024b6c",
+    "url": "/static/js/runtime-main.e20206d0.js"
+  },
+  {
+    "revision": "e439b26b28791a72f2684d0e3a1e95a6",
+    "url": "/static/media/01-calzado.e439b26b.jpg"
+  },
+  {
+    "revision": "38031f0f3ae07736d56c69a0b670dc83",
+    "url": "/static/media/02-calzado-zeus.38031f0f.jpg"
+  },
+  {
+    "revision": "5948894a24eed73ab517b12a46f81f05",
+    "url": "/static/media/fondo-nosotros.5948894a.jpg"
+  },
+  {
+    "revision": "593661dd370608d6c8b48dc124a36a05",
+    "url": "/static/media/image-02.593661dd.jpg"
+  },
+  {
+    "revision": "2e96fcbdd8e44165452a6a48e3e92522",
+    "url": "/static/media/logo.2e96fcbd.png"
+  },
+  {
+    "revision": "e8f01e4317fbced65c9fcca422ecb66d",
+    "url": "/static/media/rex.e8f01e43.jpg"
+  },
+  {
+    "revision": "88359344c402a3650857662fb27e511c",
+    "url": "/static/media/thor.88359344.jpg"
+  },
+  {
+    "revision": "95c9a21fd75296e0909070a0b0411f30",
+    "url": "/static/media/zapatos-main.95c9a21f.jpg"
+  }
+]);
