2b69d54006bc120f74e43d69676612c2c4fbbff2
CristhianLaurente <frontend.crstn@gmail.com>
2020-01-29 16:13:01 -0500

    Updates

---
+++ precache-manifest.908afc6e52faf39a18ac94743b9ea9e8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa44fdf67685799e790024993ca7d35",
+    "url": "/sandder/index.html"
+  },
+  {
+    "revision": "75eafdb0c3eb911625cb",
+    "url": "/sandder/static/css/main.734999e0.chunk.css"
+  },
+  {
+    "revision": "0f160391b73366862ffa",
+    "url": "/sandder/static/js/2.a40a9776.chunk.js"
+  },
+  {
+    "revision": "c732d40cf9f8177141935203a016d18b",
+    "url": "/sandder/static/js/2.a40a9776.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75eafdb0c3eb911625cb",
+    "url": "/sandder/static/js/main.c1b17929.chunk.js"
+  },
+  {
+    "revision": "1839fe50d3dec3485e8a",
+    "url": "/sandder/static/js/runtime-main.00df4bbb.js"
+  },
+  {
+    "revision": "e439b26b28791a72f2684d0e3a1e95a6",
+    "url": "/sandder/static/media/01-calzado.e439b26b.jpg"
+  },
+  {
+    "revision": "38031f0f3ae07736d56c69a0b670dc83",
+    "url": "/sandder/static/media/02-calzado-zeus.38031f0f.jpg"
+  },
+  {
+    "revision": "5948894a24eed73ab517b12a46f81f05",
+    "url": "/sandder/static/media/fondo-nosotros.5948894a.jpg"
+  },
+  {
+    "revision": "593661dd370608d6c8b48dc124a36a05",
+    "url": "/sandder/static/media/image-02.593661dd.jpg"
+  },
+  {
+    "revision": "2e96fcbdd8e44165452a6a48e3e92522",
+    "url": "/sandder/static/media/logo.2e96fcbd.png"
+  },
+  {
+    "revision": "e8f01e4317fbced65c9fcca422ecb66d",
+    "url": "/sandder/static/media/rex.e8f01e43.jpg"
+  },
+  {
+    "revision": "f33b4847a0f12cf13f9aea3ae7380f46",
+    "url": "/sandder/static/media/source.f33b4847.gif"
+  },
+  {
+    "revision": "88359344c402a3650857662fb27e511c",
+    "url": "/sandder/static/media/thor.88359344.jpg"
+  },
+  {
+    "revision": "95c9a21fd75296e0909070a0b0411f30",
+    "url": "/sandder/static/media/zapatos-main.95c9a21f.jpg"
+  }
+]);
